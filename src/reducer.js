38671fcce637a8e3422d63785882e2ec30231fe3
superhackerboy <mark@superhackerman.com>
2020-10-07 12:27:50 -0400

    refactor

--- src/reducer.js
+++ src/reducer.js
@@ -4,13 +4,13 @@ const initialState = {
   results: [],
 };
 
-export default (state = initialState, action) => {
-  if (action.type === "ADD") {
+export default (state = initialState, { type, searchQuery, results }) => {
+  if (type === "ADD") {
     return {
       ...state,
       dateSearched: Date.now(),
-      searchQuery: action.searchQuery,
-      results: action.results,
+      searchQuery,
+      results,
     };
   } else return state;
 };
